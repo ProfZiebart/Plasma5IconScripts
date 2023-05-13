89e60a0a63062fbc459db9d30aa25c9a6bd2ee62
Jan R <62288610+ProfZiebart@users.noreply.github.com>
2023-05-13 19:43:32 +0200

    Update plasmasaveicons.sh
    I zipped all the Icons, too

--- plasmasaveicons.sh
+++ plasmasaveicons.sh
@@ -4,6 +4,7 @@ resolution=$( xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/
 echo $resolution detected
 # copy the current plasma config to one with the resolution appendix
 cp ~/.config/plasma-org.kde.plasma.desktop-appletsrc ~/.config/plasma-org.kde.plasma.desktop-appletsrc.$resolution
+tar zcvf /home/picard/.config/plasmaicons.$resolution.tar.gz /home/picard/.local/share/plasma_icons/
 echo Icon Positions saved:
 ls ~/.config/plasma-org.kde.plasma.desktop-appletsrc* -la
 
